fcff64559338f64f31f4fe0366b21222dbc07b05
ayyoubbenslimane00@gmail.com <165728310+AyoubBenSlimane98@users.noreply.github.com>
2024-05-29 10:11:42 +0100

    v.01

--- src/index.mjs
+++ src/index.mjs
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+//for localhost 
 app.use(
   cors({
     origin: 'http://localhost:3000',
